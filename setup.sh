7948d51cff6d0ba8c9bf79d1f2da5fb8e921051f
FridrichMethod <zhaoyangli@stanford.edu>
2026-02-04 20:16:12 -0800

    Update pyproject.toml to add new dependencies including biopython, mdanalysis, pandas, polars, and scipy; enhance setup.sh to streamline environment setup with CUDA and AlphaFold installations, while removing redundant library installations.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # This script sets up the environment for `pdmodels`
 
 # Create a conda environment for `pdmodels`
-# conda create -n pdmodels python=3.12 --yes
+# conda create -n pdmodels python=3.12 -y
 # conda activate pdmodels
 
 # Enable AlphaFold for WSL2
@@ -16,30 +16,37 @@ export TF_FORCE_GPU_ALLOW_GROWTH="true"
 export LIBRARY_PATH=$CONDA_PREFIX/lib:$LIBRARY_PATH
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
+# Set up CUDA environment variables
+export CUDA_HOME=$CONDA_PREFIX
+export CUDACXX=$CUDA_HOME/bin/nvcc
+export PATH=$CUDA_HOME/bin:$PATH
+
 # Set up conda environment variables for AlphaFold3
 mv "$CONDA_PREFIX"/compiler_compat/ld{,.bak}
+
+# Install CUDA toolkit matching PyTorch (cu128)
+conda install -c nvidia cuda-toolkit=12.8 -y
 
 # Install uv and set up Python 3.12
 pip install uv
 
 # Install PyMOL
-conda install -c schrodinger -c conda-forge pymol --yes
-# Update SQLite to fix issue with Jupyter after installing PyMOL
-conda update -c conda-forge sqlite --yes
+conda install -c conda-forge glew=2.1.0 -y
+conda install -c schrodinger vtk-m=1.8.0 -y
+conda install -c conda-forge -c schrodinger pymol-bundle -y
 
-# Install AlphaFold and AlphaFold3
-uv pip install git+https://github.com/google-deepmind/alphafold.git
-uv pip install git+https://github.com/google-deepmind/alphafold3.git
+# Install multiple sequence alignment tools
+conda install -c conda-forge -c bioconda kalign hhsuite hmmer blast mmseqs2 -y
 
-# Update jax and related libraries
-uv pip install jax[cuda12] --upgrade
-uv pip install dm-haiku --upgrade
-uv pip install triton --upgrade
-uv pip install jax-triton --upgrade
-uv pip install flax --upgrade
-uv pip install deepspeed --upgrade
+# Install AlphaFold
+uv pip install git+https://github.com/FridrichMethod/alphafold.git@dev
+
+# Install AlphaFold3
+conda install -c conda-forge pdbfixer -y
+uv pip install git+https://github.com/FridrichMethod/alphafold3.git@dev
 
 # Install PyTorch Geometric and its dependencies
+pip install torch==2.8.0 --index-url https://download.pytorch.org/whl/cu128
 uv pip install torch_geometric
 uv pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.8.0+cu128.html
 
@@ -53,10 +60,6 @@ EOF
 
 # Install OpenFold
 uv pip install git+https://github.com/aqlaboratory/openfold.git --no-build-isolation
-
-# Update all CUDA libraries
-uv pip install torch --upgrade
-uv pip install nvidia-cudnn-cu12 --upgrade
 
 # Install the pdmodels package
 uv pip install -e .[dev,mypy]
